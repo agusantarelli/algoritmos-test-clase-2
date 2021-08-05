f27545900148f9adf590e5541336d97efb2d301d
agusantarelli <santarelliagustina98@gmail.com>
2021-08-05 11:43:43 -0300

    Function and test of List completed

--- src/main/java/com/redbeeacademy/algoritmostest/Lista/Lista.java
+++ src/main/java/com/redbeeacademy/algoritmostest/Lista/Lista.java
@@ -8,39 +8,88 @@ Realizar los siguientes métodos:
 5) Eliminar la posicion donde se encuentre el valor maximo de la lista
 6) Dadas dos listas, crear una nueva con los elementos que se repitan en ambas
  */
-
-import java.util.List;
+import java.lang.Integer;
+import java.util.*;
+import java.lang.Double;
 
 public class Lista {
 
     public static List<Integer> guardarEnLista(Integer numero){
-        //TODO -> implement me
-        return null;
+        List<Integer> number = new ArrayList<Integer>();
+        if(numero == null){
+            number.add(0);
+        }else{
+            number.add(numero);
+        }
+        return number;
     }
 
     public static List<Integer> intercambiar(List<Integer> lista){
-        //TODO -> implement me
-        return null;
+        List<Integer> copy = new ArrayList<>();
+        for(int i = 0;i<lista.size();i++){
+            if(lista.get(i) != null){
+                copy.add(lista.get(i));
+            }
+        }
+        Integer numAux = copy.get(2);
+        copy.set(2,copy.get(4));
+        copy.set(4,numAux);
+        return copy;
     }
 
     public static List<Integer> agregarElementoAlInicio(List<Integer> lista, Integer numero){
-        //TODO -> implement me
-        return null;
+        List<Integer> copy = new ArrayList<>();
+        for(int i = 0;i<lista.size();i++){
+            if(lista.get(i) != null){
+                copy.add(lista.get(i));
+            }
+        }
+        copy.add(0,numero);
+        return copy;
     }
 
     public static Double promedioLista(List<Integer> lista){
-        //TODO -> implement me
-        return null;
+        Double sum = 0.0;
+        Double prom = 0.0;
+        List<Integer> copy = new ArrayList<>(lista);
+        copy.removeAll(Collections.singleton(null));
+        for(int i = 0;i<copy.size();i++){
+                sum += copy.get(i);
+        }
+        prom = sum/copy.size();
+        return prom;
     }
 
     public static List<Integer> eliminarMaximo(List<Integer> lista){
-        //TODO -> implement me
-        return null;
+        List<Integer> copy = new ArrayList<>(lista);
+        copy.removeAll(Collections.singleton(null));
+        Integer max = 0;
+        Integer position = 0;
+        for(int i = 0;i<copy.size();i++){
+            if(copy.get(i) > max){
+                max = copy.get(i);
+            }
+        }
+        copy.remove(max);
+        return copy;
     }
 
     public static List<Integer> repetidos(List<Integer> a, List<Integer> b){
-        //TODO -> implement me
-        return null;
+        List<Integer> repeat = new ArrayList<>();
+        if(a.size()<b.size()){
+            for(int i = 0;i<a.size();i++){
+                if(a.contains(b.get(i))){
+                    repeat.add(b.get(i));
+                }
+            }
+        }else{
+            for(int i = 0;i<b.size();i++){
+                if(b.contains(a.get(i))){
+                    repeat.add(a.get(i));
+                }
+            }
+        }
+        return repeat;
     }
 
 }
